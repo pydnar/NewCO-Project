2f5a6a129a0eedb52f5ce68a1edc5d4c4884058d
Simmigon <simmigon.flagg@gmail.com>
2018-12-15 11:39:20 -0500

    Fix UI issues

---
+++ public/assets/js/scanner.js
@@ -0,0 +1,22 @@
+ $("#assetcheck").click(function () {
+    
+    alert ("you are checking the assets type!");
+    var newbarcode = $("#isbn_input").val().trim();
+
+     event.preventDefault();
+     console.log(newbarcode);
+
+    $.ajax("/api/scanner/" + newbarcode, {
+      type: "GET"
+    }).then(function (r) {
+      console.log(r);
+      alert(r);
+      var rshow=JSON.stringify(r);
+      var rshowonpage=JSON.parse(rshow);
+      console.log(rshow);
+      $('#barcodetyperesultp').html(rshow);
+     //
+      //$('#barcodetyperesult').append(rshow);
+      //location.reload();
+    });
+  });
